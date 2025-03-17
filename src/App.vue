7554d56633f43d58a322aaf015f74805567fc624
AdamRB208 <adamrborup@gmail.com>
2025-03-17 14:04:59 -0600

    init, updated app.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+// JS
+<script setup>
+import { computed } from 'vue'
+import { AppState } from './AppState.js'
+
+
+</script>
+
+
+// HTML
+<template>
+<div class="section">
+
+</div>
+</template>
+
+
+// styles
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
