df51a2325e24003e0c5c174a9edf4e880c4b3937
AdamRB208 <adamrborup@gmail.com>
2025-03-17 16:05:40 -0600

    created upgrades model

---
+++ src/models/Upgrades.js
@@ -0,0 +1,19 @@
+export class ClickUpgrades {
+  constructor(data) {
+    this.id = data.id
+    this.name = data.name
+    this.price = data.price
+    this.quantity = data.quantity
+    this.mineAbility = data.mineAbility
+  }
+}
+
+export class AutoUpgrades {
+  constructor(data) {
+    this.id = data.id
+    this.name = data.name
+    this.price = data.price
+    this.quantity = data.quantity
+    this.mineAbility = data.mineAbility
+  }
+}
